66483bd599146ff07a7db98459b5d99ef949ffe0
vivekascoder <vivekascoder@gmail.com>
2021-04-09 14:01:20 +0530

    Updated

---
+++ startpage/loadSearchEngines.js
@@ -0,0 +1,11 @@
+var searchEngines = {
+    yt: { icon: " ", url: "https://www.youtube.com/results?search_query=" },
+    g: { icon: " ", url: "https://www.google.com/search?q=" },
+    gt: { icon: " ", url: "https://github.com/search?q=" },
+    red: { icon: " ", url: "https://www.reddit.com/search/?q=" },
+    dj: { icon: " ", url: "https://docs.djangoproject.com/en/3.1/search/?q=" },
+    py: { icon: " ", url: "https://docs.python.org/3/search.html?q=" },
+    tw: { icon: " ", url: "https://twitter.com/search?q=" },
+    arch: { icon: " ", url: "https://wiki.archlinux.org/index.php?search=" },
+    js: { icon: " ", url: "https://developer.mozilla.org/en-US/search?q=" },
+}
--- startpage/script.js
+++ startpage/script.js
@@ -1,38 +1,89 @@
 // Some JS {Bloated} for startpage.
 
-var query_url = "https://www.google.com/search?q="
-var searchEngines = {
-    yt: {icon: " ", url: "https://www.youtube.com/results?search_query="},
-    g: {icon: " ", url: "https://www.google.com/search?q="},
-    gt: {icon: " ", url: "https://github.com/search?q="},
-    red: {icon: " ", url: "https://www.reddit.com/search/?q="},
-    dj: {icon: " ", url: "https://docs.djangoproject.com/en/3.1/search/?q="},
-    py: {icon: " ", url: "https://docs.python.org/3/search.html?q="},
-    tw: {icon: " ", url: "https://twitter.com/search?q="},
-    arch: {icon: " ", url: "https://wiki.archlinux.org/index.php?search="},
-    js: {icon: " ", url: "https://developer.mozilla.org/en-US/search?q="},
+const ls = {
+    get: function(key) {
+        let latestSECode = localStorage.getItem(key)
+        return latestSECode
+    },
+    set: function(key, value) {
+        localStorage.setItem(key, value)
+    }
 }
-var search = document.querySelector("#query")
-var logo = document.querySelector("#search_logo")
-var query = ""
 
-document.addEventListener("keydown", (e) => {
+
+let query_url
+let search = document.querySelector("#query")
+let logo = document.querySelector("#search_logo")
+let autoComplete = document.querySelector("#auto-complete")
+let query = ""
+let currentCode = ""
+
+const engineHtmlGenerator = (query, searchEngines) => {
+    let engineTemplate = ""
+    let filteredItems = Object.keys(searchEngines).filter((value) => {
+        return value.startsWith(query.slice(1))
+    })
+
+    for (let i = 0; i < filteredItems.length; i++) {
+        let key = filteredItems[i]
+        let restString = (":" + key).replace(query, "")
+        engineTemplate += `
+        <div class="engine">
+            <span class="logo">${searchEngines[key].icon}</span>
+            <span class="name">[<span class="selected">${query}</span>${restString}] ${searchEngines[key].url}</span>
+        </div>
+        `
+    }
+    return engineTemplate
+}
+
+search.focus()
+search.addEventListener("keydown", (e) => {
     if (e.code == "Tab") {
         e.preventDefault()
     }
 })
 
-document.addEventListener("keyup", (e) => {
+search.addEventListener("keyup", (e) => {
     query = search.value
-    if (e.code == "Enter") {
+    autoComplete.innerHTML = ""
+    if (search.value.startsWith(":") && !search.value.match(" ")) {
+        autoComplete.innerHTML = engineHtmlGenerator(search.value, searchEngines)
+    }
+
+    if (e.code == "Enter" && search.value) {
         query_url += search.value.replaceAll(" ", "+")
+
+        let currentCache = JSON.parse(ls.get(`cache_${currentCode}`))
+        if(!currentCache) currentCache = []
+        currentCache.push(search.value)
+        currentCache = JSON.stringify(currentCache)
+        ls.set(`cache_${currentCode}`, currentCache)
+
         window.location = query_url
     }
     if (e.code == "Tab") {
-        if (Object.keys(searchEngines).find((value) => value == query.slice(1))){
-            logo.innerHTML = searchEngines[query.slice(1)].icon
-            query_url = searchEngines[query.slice(1)].url
+        let code = query.slice(1)
+        currentCode = code
+        let engineData = searchEngines[code]
+
+        if (engineData) {
+            ls.set('latest', query.slice(1))
+            logo.innerHTML = engineData.icon
+            query_url = engineData.url
             search.value = ""
+            autoComplete.innerHTML = ""
         }
     }
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    let latestEngineData = searchEngines[ls.get('latest')]
+    let defaultEngineData = searchEngines[ls.get('default')]
+
+    currentCode = ls.get('default') || ls.get('latest')
+    let firstEngine = defaultEngineData || latestEngineData
+
+    query_url = firstEngine?.url || 'https://www.google.com/search?q='
+    logo.innerHTML = firstEngine.icon
 })
